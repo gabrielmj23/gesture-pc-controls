ce6f9b79c97803121514767953c80b679646a471
gabrielmj23 <gabrielmj2312@gmail.com>
2023-08-10 19:47:57 -0400

    Change volume/brightness with right hand, change active window with left

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import math
 import numpy as np
+import pygetwindow as gw
 import screen_brightness_control as sbc
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
@@ -12,11 +13,15 @@ def rescale(img, scale=1.5):
     new_height = int(img.shape[0] * scale)
     return cv.resize(img, (new_width, new_height), interpolation=cv.INTER_AREA)
 
-def is_horizontal_enough(pt1, pt2):
-    """Checks if line defined by pt1 and pt2 is horizontal enough to control volume.\n
-    Points are given as two-value tuples"""
-    m = abs((pt2[1] - pt1[1]) / (pt2[0] - pt1[0] + 0.1))
-    return m < 1
+def is_right_hand(thumb, pinky):
+    """Uses thumb and pinky finger landmarks to determine if looking at a right hand or not.
+
+    Only works when palm is shown on a generally normal position, otherwise gives opposite results"""
+    return thumb[0] <= pinky[0]
+
+def slope(pt1, pt2):
+    """Gets slope of line connecting pt1 and pt2 given as two-value tuples"""
+    return abs((pt2[1] - pt1[1]) / (pt2[0] - pt1[0] + 0.1))
 
 def main():
     # Set up audio management
@@ -26,10 +31,17 @@ def main():
     )
     volume = interface.QueryInterface(IAudioEndpointVolume)
     min_vol, max_vol, _ = volume.GetVolumeRange()
+    vol = volume.GetMasterVolumeLevel()
+
+    # Variables for changing windows
+    window_change_delay = 0
+    window_index = 0
 
     capture = cv.VideoCapture(0)
-    vol = volume.GetMasterVolumeLevel()
     while True:
+        if window_change_delay > 0:
+            window_change_delay -= 1
+
         _, frame = capture.read()
         if frame is not None:
             frame = rescale(cv.flip(frame, 1))
@@ -40,24 +52,46 @@ def main():
             cv.putText(frame, f'Brightness: {sbc.get_brightness()[0]}', (10, 60), cv.QT_FONT_NORMAL, 1, (0, 0, 0), 2)
 
             hand_landmarks = get_landmarks_list(frame)
-            positions = get_landmarks_list(frame, format=True)
-            
             if hand_landmarks is not None and len(hand_landmarks) > 0:
                 # Draw landmarks
                 for lms in hand_landmarks:
                     drawModule.draw_landmarks(frame, lms, handsModule.HAND_CONNECTIONS)
                 # Get significant landmarks
-                thumb, finger = positions[4], positions[8]
+                positions = []
+                for hand in hand_landmarks:
+                    for lm in hand.landmark:
+                        positions.append((int(lm.x * frame.shape[1]), int(lm.y * frame.shape[0])))
+                thumb, finger, finger_base = positions[4], positions[8], positions[5]
                 length = math.hypot(finger[0] - thumb[0], finger[1] - thumb[1])
-                # Check horizontal-ness
-                if (is_horizontal_enough(thumb, finger)):
-                    # Control volume
-                    vol = np.interp(length, [10, 250], [min_vol, max_vol])
-                    volume.SetMasterVolumeLevel(vol, None)
+                
+                # Handle volume and brightness with the right hand
+                if (is_right_hand(thumb, positions[20])):
+                    if (slope(thumb, finger) <= 1):
+                        # Control volume
+                        vol = np.interp(length, [10, 250], [min_vol, max_vol])
+                        volume.SetMasterVolumeLevel(vol, None)
+                    else:
+                        # Control brightness
+                        brightness = np.interp(length, [10, 250], [0, 100])
+                        sbc.set_brightness(brightness)
+                # Change windows when using left hand
                 else:
-                    # Control brightness
-                    brightness = np.interp(length, [10, 250], [0, 100])
-                    sbc.set_brightness(brightness)
+                    index_slope = slope(finger_base, finger)
+                    if window_change_delay == 0 and abs(index_slope) <= 2.5:
+                        window_change_delay = 30
+                        windows = gw.getAllWindows()
+                        if window_index > len(windows):
+                            window_index = 0
+                        try:
+                            windows[window_index].activate()
+                        except:
+                            windows[window_index].minimize()
+                            windows[window_index].restore()
+                        if index_slope >= 0:
+                            window_index = (window_index + 1) % len(windows)
+                        else:
+                            window_index = (window_index - 1) % len(windows)
+            
             cv.imshow("Video", frame)  # Show video
             if (cv.waitKey(20) & 0xFF) == 27:
                 break
--- tracking.py
+++ tracking.py
@@ -6,16 +6,8 @@ handsModule = mp.solutions.hands
 drawModule = mp.solutions.drawing_utils
 detector = handsModule.Hands(max_num_hands=1, min_detection_confidence=0.7)
 
-def get_landmarks_list(img, format=False):
+def get_landmarks_list(img):
     """Gets list of significant hand landmarks in a given BGR image"""
     rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     results = detector.process(rgb)
-    landmarks_list = results.multi_hand_landmarks
-    if not format or landmarks_list is None:
-        return landmarks_list  # Return normalized coordinates without landmark id
-    formatted_list = []
-    for hand in landmarks_list:
-        for id, lm in enumerate(hand.landmark):
-            formatted_list.append((int(lm.x * img.shape[1]), int(lm.y * img.shape[0])))
-    return formatted_list
-    
+    return results.multi_hand_landmarks
